5ef7f9c46bf96e9d39e0ad08022a086b62259e63
vinodselpol <vinodselpol@example.com>
2023-10-30 15:51:28 -0500

    Header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
   return (<Router>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
