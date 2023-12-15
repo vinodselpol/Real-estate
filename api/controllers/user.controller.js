621536e973c73a049b88380ae704b9fa7819f0a6
vinodselpol <vinodselpol@example.com>
2023-12-15 11:54:23 -0600

    Complete show user listings functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,7 +36,7 @@ export const updateUser = async (req, res, next) => {
 }
 
 export const deleteUser = async (req, res, next) => {
-    console.log(req.user.id, req.params.id, 'delete user')
+   
     if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
     try {
         await User.findByIdAndDelete(req.params.id)
@@ -52,12 +52,13 @@ export const deleteUser = async (req, res, next) => {
 
 
 export const getUserListings = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
  
-      try {
+    try {
         const listings = await Listing.find({ userRef: req.params.id });
         res.status(200).json(listings);
-      } catch (error) {
+    } catch (error) {
         next(error);
-      }
+    }
     
   };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,11 @@
 import {useRef, useState, useEffect} from 'react'
 import { useSelector } from 'react-redux'
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import {getDownloadURL, getStorage, list, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutStart, signOutSuccess, signOutFailure} from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { set } from 'mongoose'
 
 function Profile() {
   const {currentUser, loading, error} = useSelector((state) => state.user)
@@ -16,6 +17,8 @@ function Profile() {
   const [updateSucess, setUpdateSuccess] = useState(false)
   const dispatch = useDispatch()
   console.log(formData)
+  const [showListingsError, setShowListingsError] = useState(false)
+  const [listingsData, setListingsData] = useState([])
 
   useEffect(() => {
 
@@ -123,6 +126,33 @@ function Profile() {
 
   }
   
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false)
+      const res = await fetch(`/api/user/listings/${currentUser._id}`,{
+        method: 'GET',
+      
+      })
+      const data = await res.json()
+     
+      if(data.success == false){
+        setShowListingsError(true)
+        return
+      }
+
+      
+      setListingsData(data)
+    
+      
+    } catch (error) {
+      setShowListingsError(true)
+
+      
+    }
+
+  }
+  console.log(listingsData)
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -157,6 +187,35 @@ function Profile() {
       </div>
       <p className='text-red-700 mt-5'> {error ? error : ""}</p>
       <p className='text-green-700'> {updateSucess ? 'User is updated successfully' : ""}</p>
+
+      <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+      <p className='text-red-700 mt-5'> {showListingsError ? 'Error showing listings' : ""}</p>
+      {listingsData && listingsData.length > 0 && <div className='flex flex-col gap-4'>
+        <h1 className='text-2xl font-semibold text-center mt-7'>Your Listings</h1> 
+        {listingsData.map((listing) => {
+        return (
+          
+          <div key={listing._id} className='border p-3 rounded-lg mt-5 flex gap-4 justify-between items-center'>
+            <Link to={`/listing/${listing._id}`}>
+          <img src={listing.imageUrls[0]} alt="listing" className='w-38 h-16 object-contain mt-2' />
+          </Link>
+          <Link  className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+            <h1>{listing.name}</h1>
+            </Link>
+            <div className='flex flex-col items-center'>
+              <button className=' rounded-lg text-red-700  uppercase p-1'>Delete</button>
+              <button className=' rounded-lg text-green-700   uppercase'>Edit</button>
+              </div>
+         
+          </div>
+          
+          
+        )
+
+      })}
+
+
+    </div>}
     </div>
   )
 }
