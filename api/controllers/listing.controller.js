856539322d741b72e590aa5569a3adbb7ab78f94
vinodselpol <vinodselpol@example.com>
2023-12-15 12:46:00 -0600

    Add delete user listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -11,3 +11,16 @@ export const createListing = async (req, res, next) => {
         
     }
 }
+
+//delete the listing
+
+export const deleteListing = async (req, res, next) => {
+    try{
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json('Listing has been deleted!')
+
+    } catch (error) {
+        next(error)
+    }
+
+ }
